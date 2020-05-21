e94f33bb3fb580582324a94ea9c6a866a58d7b29
Jakarea <jakareaparvez@gmail.com>
2020-05-21 18:25:01 +0600

    new portfolio about page

--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer = () => (
-  <footer className="footer">
-    <div className="sub-footer  text-center">
-      <p>Copy Right © By Jakarea Parvez 2014-17 | All Rights Reserved.</p>
-    </div>
-  </footer>
-);
-export default Footer;
